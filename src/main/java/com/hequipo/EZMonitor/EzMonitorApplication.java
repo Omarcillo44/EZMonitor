b91d4726e080c8665d32a55abb564720ac1db70a
Omar Lorenzo Pacheco <omarcisho@mini-poderosa-debian.poderosa>
2024-11-09 16:42:14 -0600

    Initial commit

---
+++ src/main/java/com/hequipo/EZMonitor/EzMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.hequipo.EZMonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hequipo/EZMonitor/EzMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hequipo.EZMonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
